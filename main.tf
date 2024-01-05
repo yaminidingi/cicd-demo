96a487bb9e0f074a779f3cba0b26d81065c5c2b7
yaminidingi <yaminidingi@gmail.com>
2024-01-05 15:59:14 +0530

    testing2

--- main.tf
+++ main.tf
@@ -18,7 +18,8 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "demo_db" {
-  name    = "DEMO_DB"
-  comment = "Database for Snowflake Terraform demo"
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
 }
