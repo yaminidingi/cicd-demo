94625717508e8c7c7fe2eefbb7f07fc459fb09f3
yaminidingi <yaminidingi@gmail.com>
2024-01-05 14:36:56 +0530

    demo

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     snowflake = {
+      source = "chanzuckerberg/snowflake"
       version = "0.25.17"
-      source = registry.terraform.io/hashicorp/snowflake
     }
   }
 
